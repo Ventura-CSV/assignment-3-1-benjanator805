0df65bc006567067971f4b90281100076a75d974
NotBen <benramirez805@gmail.com>
2024-06-27 17:23:26 -0700

    main.py

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import random
 
 def main():
     number1 = random.randint(0, 100)
+    print("First number: ", number1)
     number2 = random.randint(0, 100)
+    print("Second number: ", number2)
     number3 = random.randint(0, 100)
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    print("Third number: ", number3)
 
+    ########################################
+    if number1 <= number2 and number1 <= number3:
+        min_value = number1
+    elif number2 <= number1 and number2 <= number3:
+        min_value = number2
+    else: number3
+
+    print("The minimum value is: ", min_value)
+    ########################################
+    
     ########################################
     # Do not delete the return statement
     ########################################
